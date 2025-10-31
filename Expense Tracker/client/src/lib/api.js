60f28899054cf2569b50efed39aad8cb897048b6
nilesh98199 <project.nilesh24@gmail.com>
2025-10-31 12:49:58 +0530

    Change token key for authorization header

--- Expense Tracker/client/src/lib/api.js
+++ Expense Tracker/client/src/lib/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("budgetmate_token");
+  const token = localStorage.getItem("ExpenseTracker_token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
